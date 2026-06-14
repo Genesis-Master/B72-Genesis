8a53d0e807ee46e541a851f9118783ddf750d47a
Genesis-Master <info@ospreonllc.com>
2026-06-14 18:39:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ img_data_main = ""
 uploaded = st.file_uploader("Upload images", accept_multiple_files=True) 
 for file in uploaded:
    file_bytes_data = file.read()
-        File "/mount/src/b72-genesis/app.py", line 16
+    File "/mount/src/b72-genesis/app.py", line 16
           byte_signatures[file_name] = f"SIG-{len(file_bytes_data)}-{file_name[:3].upper()}"
  
          encoded = base64.b64encode(file_bytes_data).decode('utf-8')
